f57456d457bd93e5298eb9da77a573b9fa3777f5
Владислав <captain.obvious1995@yandex.ru>
2016-10-05 12:46:11 +0300

    Third task completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task2
 {
@@ -11,47 +8,18 @@ namespace Task2
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-            //int row = input[0];
             char place = input.Last();
             int row = Convert.ToInt32(input.Trim(place));
-            /*
-            if (row <= 2)
-            {
-                if (place == 'A' || place == 'D')
-                    Console.WriteLine("window");
-                else Console.WriteLine("aisle");
-            }
-            if (row > 2 && row <= 20)
-            {
-                if (place == 'A' || place == 'F')
-                    Console.WriteLine("window");
-                else Console.WriteLine("aisle");
-            }
-            if (row > 20 && row <= 65)
-            {
-                if (place == 'A' || place == 'K')
-                    Console.WriteLine("window");
-                else
-                {
-                    if (place == 'B' || place == 'E' || place == 'F' || place == 'J')
-                        Console.WriteLine("neither");
-                    else Console.WriteLine("aisle");
-                }
-            }*/
-
-            //window
             if ((row <= 2 && (place == 'A' || place == 'D'))
                 || ((row > 2 && row <= 20) && (place == 'A' || place == 'F'))
                 || ((row > 20 && row <= 65) && (place == 'A' || place == 'K')))
                 Console.WriteLine("window");
-            //aisle
             if ((row <= 2 && !(place == 'A' || place == 'D'))
                 || ((row > 2 && row <= 20) && !(place == 'A' || place == 'F'))
                 || ((row > 20 && row <= 65) &&
                         !(place == 'A' || place == 'K' || place == 'B'
                         || place == 'E' || place == 'F' || place == 'J')))
                 Console.WriteLine("aisle");
-            //neither
             if (row > 20 && row <= 65 && (place == 'B' || place == 'E' || place == 'F' || place == 'J'))
                 Console.WriteLine("neither");
             Console.ReadKey();
---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            string[] inpfields = new string[count - 1];
+            inpfields = Console.ReadLine().Split(' ');
+            List<int> fields = new List<int>();
+            foreach (var i in inpfields)
+                fields.Add(Convert.ToInt32(i));
+            int maxsum = 0;
+            int middlesection = 0;
+            for (int i = 0; i <= fields.Count-3; i++)
+            {
+                int sum = fields[i] + fields[i + 1] + fields[i + 2];
+                if (sum > maxsum)
+                {
+                    maxsum = sum;
+                    middlesection = i + 2;
+                }
+            }
+            Console.WriteLine($"{ maxsum} {middlesection}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
