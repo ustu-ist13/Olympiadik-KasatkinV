f7634766359d8d74ab4aa1a9df02d96d9f3c593e
Владислав <captain.obvious1995@yandex.ru>
2016-10-05 17:07:05 +0300

    Fifth task completed

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countoftests = Convert.ToInt32(Console.ReadLine());
+            var inp = Console.ReadLine();
+            //The Machinegunners played home/away game, scored x goals, and conceded y goals
+            bool ishome = inp[26] == 'h';
+            int scoredgoals = 0;
+            int concededgoals = 0;
+        }
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inp = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(inp[0]);
+            int k = Convert.ToInt32(inp[1]);
+            inp = Console.ReadLine().Split(' ');
+            List<int> bumbums = new List<int>();
+            foreach (var i in inp)
+                bumbums.Add(Convert.ToInt32(i));
+            int bbslast = 0;
+            int drdslast = 0;
+            foreach (var b in bumbums)
+            {
+                if (b > k)
+                    bbslast += b - k;
+                else
+                    drdslast += k - b;
+            }
+            Console.WriteLine($"{bbslast} {drdslast}");
+            Console.ReadKey();
+        }
+    }
+}
