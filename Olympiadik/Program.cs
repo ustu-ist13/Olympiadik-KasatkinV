988d24b608d147e82424ff3c5bf18dd48af54f6b
Владислав <captain.obvious1995@yandex.ru>
2016-10-05 10:55:46 +0300

    First task completed

--- Olympiadik/Program.cs
+++ Olympiadik/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Olympiadik
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int f = 0;
+            while (f >= 11 || f < 1)
+            {
+                f = Convert.ToInt32(Console.ReadLine());
+            }
+            if (((4*60) / 45) >= 12 - f)
+            {
+                Console.WriteLine("YES");
+            }
+            else Console.WriteLine("NO");
+            Console.ReadKey();
+
         }
     }
 }
